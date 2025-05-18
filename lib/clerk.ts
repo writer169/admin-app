0b5b345da3a255f30bda81c29d6562420839282d
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-18 13:16:57 +0500

    Update clerk.ts

--- lib/clerk.ts
+++ lib/clerk.ts
@@ -1,6 +1,6 @@
 import { clerkClient } from '@clerk/nextjs/server';
 
-export async function getUserEmail(userId: string): Promise<string | for null> {
+export async function getUserEmail(userId: string): Promise<string | null> {
   try {
     const user = await clerkClient.users.getUser(userId);
     return user.emailAddresses[0]?.emailAddress || null;
