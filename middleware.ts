6b06448a1f7ada2889c6fe6eccca0dab845b7fcb
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-18 13:51:51 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Стандартная конфигурация из документации Clerk
+    '/((?!.*\\..*|_next).*)', // Исключает файлы с точкой в имени и Next.js internals
+    '/',                      // Включает корневой путь
+    '/(api|trpc)(.*)',        // Включает все API роуты
   ],
 };
