be06447053a657679128744caeec6fe66a591193
writer169 <148839750+writer169@users.noreply.github.com>
2025-05-18 14:01:08 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,17 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ['/api/webhook'],
+  // Routes that can always be accessed, and have
+  // no authentication information
+  ignoredRoutes: [],
+});
 
 export const config = {
-  matcher: [
-    // Стандартная конфигурация из документации Clerk
-    '/((?!.*\\..*|_next).*)', // Исключает файлы с точкой в имени и Next.js internals
-    '/',                      // Включает корневой путь
-    '/(api|trpc)(.*)',        // Включает все API роуты
-  ],
+  // Matcher ignoring `/_next/` and `/api/` routes
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
